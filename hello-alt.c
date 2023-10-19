e712aedd1472a66168a2095f0949284fb27923f6
root <schulaluke@gmail.com>
2023-10-19 10:06:58 -0700

    created file and workd on exercise 1.1-1.2

---
+++ hello-alt.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/** 
+ * alternative way to write "hello world" with printf
+ */
+main()
+{
+	print("hello, ");
+	printf("world");
+	printf("\n");
+}
---
+++ hello.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+/** Program to print "hello, world"
+ */
+
+int main(void)
+{	
+	printf("hello, world\n");
+}
